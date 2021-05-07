096a4179e7969b8a9172ea50919b92681ea3cd69
Action <theactionbreaker@gmail.com>
2021-05-07 19:26:00 +0000

    Added simple slash command

--- abeille.py
+++ abeille.py
@@ -7,6 +7,7 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
 from dotenv import load_dotenv
+from cogs.misc import guild_ids
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,8 +23,7 @@ DESCRIPTION = "Abeille"
 bot = commands.Bot(command_prefix=commands.when_mentioned, description=DESCRIPTION)
 bot.remove_command("help")
 
-slash = SlashCommand(bot)
-
+slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 
 if __name__ == "__main__":
     p = pathlib.Path(__file__).parent / COGS_DIR
--- cogs/misc.py
+++ cogs/misc.py
@@ -1,15 +1,24 @@
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
+import configparser
+import pathlib
+
+guild_ids = []
+config = configparser.ConfigParser(allow_no_value=True)
+p = pathlib.Path(__file__).parent.parent
+config.read(p / "config.ini")
+for guild_id_str in config["Tracked"]:
+    guild_ids.append(int(guild_id_str))
 
 
 class Misc(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="ping")
+    @cog_ext.cog_slash(name="bzz", description="Bzz bzz ! 🐝", guild_ids=guild_ids)
     async def ping(self, ctx: SlashContext):
-        await ctx.reply("🐝")
+        await ctx.send("Je vais bientôt marcher avec des commandes slash ! 🐝")
 
     async def cog_command_error(self, ctx: commands.Context, error):
         await ctx.reply(f"Quelque chose s'est mal passée ({error}). 🐝")
