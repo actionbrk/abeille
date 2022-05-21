f22ac29eae4c1e95b1905b99f6137284ecd14bf1
Action <theactionbreaker@gmail.com>
2022-05-22 00:00:01 +0200

    Thread safety with multiple databases

--- models/message.py
+++ models/message.py
@@ -1,5 +1,6 @@
 from peewee import BigIntegerField, DateTimeField, Model, TextField, CharField
 from playhouse.sqlite_ext import FTS5Model, FTSModel, SearchField, RowIDField
+from playhouse.shortcuts import ThreadSafeDatabaseMetadata
 
 
 class Message(Model):
@@ -12,6 +13,9 @@ class Message(Model):
     content = TextField()
     attachment_url = TextField(null=True)
 
+    class Meta:
+        model_metadata_class = ThreadSafeDatabaseMetadata
+
 
 class MessageIndex(FTS5Model):
     """Message Index"""
@@ -20,6 +24,8 @@ class MessageIndex(FTS5Model):
 
     class Meta:
         """MessageIndex Meta"""
+
+        model_metadata_class = ThreadSafeDatabaseMetadata
 
         options = {
             "content_rowid": Message.message_id,
