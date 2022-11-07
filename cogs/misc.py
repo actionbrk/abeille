dd765370505352aefb8606ff349878116a19ecbb
Action <theactionbreaker@gmail.com>
2022-11-07 23:40:54 +0100

    Info command

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,17 +2,16 @@
 
 import logging
 import os
+import pathlib
+import platform
+import sqlite3
 from typing import Optional
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from common.utils import DEV_GUILD
-
-salt = os.getenv("SALT").encode()  # type:ignore
-iterations = int(os.getenv("ITER"))  # type:ignore
-hash_name: str = os.getenv("HASHNAME")  # type:ignore
+dbs_folder_path = os.getenv("DBS_FOLDER_PATH")
 
 
 class Misc(commands.Cog):
@@ -21,10 +20,43 @@ class Misc(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="bzz", description="Bzz bzz (ping) ! 🐝")
-    async def ping(self, interaction: discord.Interaction):
-        """Ping"""
-        await interaction.response.send_message("Je fonctionne ! 🐝")
+    @app_commands.command(
+        name="info", description="Informations sur l'instance d'Abeille 🐝"
+    )
+    @app_commands.guild_only()
+    async def info(self, interaction: discord.Interaction):
+        """Shown instance information"""
+        size_bytes = os.path.getsize(
+            pathlib.Path(dbs_folder_path) / f"{interaction.guild_id}.db"
+        )
+        size_go = f"{size_bytes/(1024*1024*1024):.1f}"
+        owner = self.bot.get_user(self.bot.owner_id)
+        bot_name = f"{self.bot.user.name}#{self.bot.user.discriminator}"
+
+        embed = discord.Embed(
+            title=f"{size_go} GB d'espace est utilisé pour *{interaction.guild.name}*",
+            description=f"Il s'agit des données enregistrées par Abeille lui permettant de fonctionner sur ce serveur.",
+        )
+        embed.add_field(
+            name=f"Informations du système exécutant {bot_name}",
+            value=f"""
+                Système d'exploitation : {platform.platform()}
+                SQLite {sqlite3.sqlite_version}
+                discord.py {discord.__version__}
+                """,
+            inline=False,
+        )
+        embed.set_author(
+            name=bot_name,
+            icon_url=self.bot.user.avatar.url,
+        )
+        embed.set_footer(
+            text=f"{bot_name} est maintenu par {owner.name}#{owner.discriminator}",
+            icon_url=owner.avatar.url,
+        )
+        await interaction.response.send_message(
+            content="<@168378684497985536> <@339928611937189888>", embed=embed
+        )
 
     @commands.command()
     @commands.is_owner()
@@ -34,24 +66,12 @@ class Misc(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def syncdev(self, ctx: commands.Context):
-        """Sync commands globally"""
-        await self._sync_commands(guild=DEV_GUILD)
-
-    @commands.command()
-    @commands.is_owner()
     async def clear(self, ctx: commands.Context, guild_str: Optional[str] = None):
         """Sync commands globally"""
         guild = None
         if guild_str:
             guild = discord.Object(id=int(guild_str))
         await self._clear_commands(guild=guild)
-
-    @commands.command()
-    @commands.is_owner()
-    async def cleardev(self, ctx: commands.Context):
-        """Sync commands globally"""
-        await self._clear_commands(guild=DEV_GUILD)
 
     @commands.command()
     @commands.is_owner()
