1eb23a2c550d67760bd9b5d1685450270d118035
Action <theactionbreaker@gmail.com>
2022-11-20 21:12:04 +0100

    `save` command

--- cogs/history.py
+++ cogs/history.py
@@ -5,7 +5,7 @@ from typing import List, Optional, Tuple
 import discord
 from discord.abc import Snowflake
 from discord.ext import commands
-from peewee import Database, DoesNotExist, fn, DateTimeField
+from peewee import DoesNotExist, fn
 
 from cogs.tracking import get_message, get_tracked_guild, get_tracked_guilds
 from models.message import Message
@@ -51,161 +51,123 @@ class History(commands.Cog):
                     )
                     continue
 
-                # Check if already saved messages in DB
+                # Get oldest message from DB
                 with db.bind_ctx([Message]):
-                    is_unknown_channel = (
-                        Message.get_or_none(Message.channel_id == channel.id) is None
+                    oldest_msg: Message = (
+                        Message.select()
+                        .where(Message.channel_id == channel.id)
+                        .order_by(Message.message_id)
+                        .get_or_none()
                     )
 
-                if is_unknown_channel:
+                if tracked_guild.last_saved_msg is None or oldest_msg is None:
                     logging.info(
-                        "No saved message were found for channel '%s' [%d]. Saving all messages...",
+                        "No saved message were found for channel '%s' [%d].",
+                        channel.name,
+                        channel.id,
+                    )
+                    logging.info(
+                        "Saving all messages for channel '%s' [%d]...",
                         channel.name,
                         channel.id,
                     )
                     try:
                         save_result = await self._save_from_channel(channel, count=None)
                         logging.info(save_result)
-                    except Exception:
+                    except discord.Forbidden:
                         logging.warning(
-                            "Cannot save channel '%s' [%d]", channel.name, channel.id
+                            "Saving channel '%s' [%d] is forbidden.",
+                            channel.name,
+                            channel.id,
                         )
                 else:
                     logging.info(
-                        "Saved messages were found for channel '%s' [%d]. Saving older and newer messages...",
+                        "Saved messages were found for channel '%s' [%d].",
                         channel.name,
                         channel.id,
                     )
-                    # try:
-                    # TODO: Save older and newer messages
-                    # except:
-                    #     logging.warn(
-                    #         "Cannot save channel '%s' [%d]", channel.name, channel.id
-                    # )
 
-                # try:
-                # TODO: save_result = await self._save_from_channel(channel)
-                # TODO: logging.info(save_result)
-                # except:
-                # logging.warn("Cannot save channel [%d]",channel.id)
+                    try:
+                        logging.info(
+                            "Saving older messages for channel '%s' [%d], before %s...",
+                            channel.name,
+                            channel.id,
+                            oldest_msg.timestamp,
+                        )
+                        save_result = await self._save_from_channel(
+                            channel,
+                            before=datetime.fromisoformat(str(oldest_msg.timestamp)),
+                        )
+                        logging.info(save_result)
+                        logging.info(
+                            "Saving newer messages for channel '%s' [%d], after %s...",
+                            channel.name,
+                            channel.id,
+                            tracked_guild.last_saved_msg.timestamp,
+                        )
+                        save_result = await self._save_from_channel(
+                            channel,
+                            after=datetime.fromisoformat(
+                                str(tracked_guild.last_saved_msg.timestamp)
+                            ),
+                        )
+                        logging.info(save_result)
+                    except discord.Forbidden:
+                        logging.warning(
+                            "Saving channel '%s' [%d] is forbidden.",
+                            channel.name,
+                            channel.id,
+                        )
 
-            # if tracked_guild.last_saved_msg is not None:
-            #     timestamp: datetime = datetime.fromisoformat(
-            #         tracked_guild.last_saved_msg.timestamp
-            #     )
-            #     logging.info(
-            #         "Saving new messages from guild '%d' since %s...",
-            #         tracked_guild.guild_id,
-            #         timestamp,
-            #     )
-            #     known_channels = await self._get_known_channels(tracked_guild.database)
-            #     for channel in known_channels:
-            #         save_result = await self._save_from_channel(
-            #             channel, after=timestamp
-            #         )
-            #         logging.info(save_result)
+            # Clean not found channels
+            # Does not delete channel with missing permissions
+            logging.info("Cleaning known but unavailable channels...")
+            with db.bind_ctx([Message]):
+                for known_channel_id in Message.select(Message.channel_id).distinct():
+                    known_channel = self.bot.get_channel(known_channel_id.channel_id)
+
+                    clean_this_channel = False
+
+                    # Check if channel is deleted
+                    is_channel_found = isinstance(
+                        known_channel,
+                        discord.TextChannel,
+                    )
+                    if not is_channel_found:
+                        clean_this_channel = True
+                        logging.info(
+                            "Channel [%d] is not available. Delete all messages...",
+                            known_channel_id.channel_id,
+                        )
+                    # Check if channel is not allowed for Abeille (missing permissions) or other problems
+                    else:
+                        # Channel is found but can be unaccessible
+                        try:
+                            [msg async for msg in known_channel.history(limit=1)]
+                        except discord.Forbidden:
+                            clean_this_channel = True
+                            logging.info(
+                                "Channel %s [%d] is forbidden.",
+                                known_channel.name,
+                                known_channel.id,
+                            )
+
+                    if clean_this_channel:
+                        logging.info(
+                            "Delete channel [%d] in database...",
+                            known_channel_id.channel_id,
+                        )
+                        Message.delete().where(
+                            Message.channel_id == known_channel_id.channel_id
+                        ).execute()
+                        logging.info(
+                            "Deleted channel [%d].", known_channel_id.channel_id
+                        )
+
+            logging.info("Cleaning done.")
 
         logging.info("'save' command done.")
         await ctx.send("`Done`")
-
-    @commands.command()
-    @commands.is_owner()
-    async def saveall(self, ctx: commands.Context, guild_id: int, count: int = 20):
-        """Sauvegarde les messages de tous les channels possibles à partir d'ici"""
-        save_results = {}
-        impossible_channels = []
-
-        guild = self.bot.get_guild(guild_id)
-        if guild is None:
-            await ctx.reply("Je ne trouve pas cette guild")
-            return
-
-        tracked_guild = get_tracked_guild(self.bot, guild_id)
-
-        # Détection des channels
-        for channel in guild.channels:
-            if not isinstance(channel, discord.TextChannel):
-                continue
-
-            # Si channel ignoré, passer
-            if channel.id in tracked_guild.ignored_channels_ids:
-                await ctx.send(f"Channel {channel.name} ignoré")
-                continue
-
-            try:
-                save_results[channel.name] = await self._save_from_channel(
-                    channel, count
-                )
-            except:
-                impossible_channels.append(channel)
-
-        for name, save_result in save_results.items():
-            await ctx.send(f"**{name}**\n{save_result}")
-
-        impossible_channels_str = []
-        for impossible_channel in impossible_channels:
-            impossible_channels_str.append(impossible_channel.name)
-        await ctx.send("Je n'ai pas pu enregistrer les autres channels")
-        # await ctx.send("\n".join(impossible_channels_str))
-
-    @commands.command()
-    @commands.is_owner()
-    async def saveoldall(self, ctx: commands.Context, guild_id: int, count: int = 20):
-        """Save old sur les channels connus en db"""
-        db = get_tracked_guild(self.bot, guild_id).database
-        guild = self.bot.get_guild(guild_id)
-
-        if guild is None:
-            await ctx.send("Je ne trouve pas cette guild")
-            return
-
-        # Récupérer liste channels connus
-        known_channels = await self._get_known_channels(db)
-        if not known_channels:
-            await ctx.send("Aucun channel connu, d'abord utiliser saveall ou save")
-            return
-        await ctx.send(
-            f"J'ai trouvé **{len(known_channels)}** channels connus en db..."
-        )
-
-        # Parcours channels
-        for channel in known_channels:
-            msg_bot = await ctx.send(f"J'enregistre le channel **{channel.name}**...")
-            # Récupérer le plus ancien message du channel
-            with db.bind_ctx([Message]):
-                try:
-                    oldest = (
-                        Message.select()
-                        .where(Message.channel_id == channel.id)
-                        .order_by(Message.message_id)
-                        .get()
-                    )
-                except DoesNotExist:
-                    print(f"Pas de message en db pour le channel {channel}")
-                    continue
-
-            # discord.Message correspondant
-            try:
-                oldest_msg: discord.Message = await channel.fetch_message(oldest)
-            except discord.NotFound:
-                print(
-                    f"Le plus ancien message enregistré du channel {channel} n'existe plus"
-                )
-            except discord.Forbidden:
-                print(f"Problème de droit pour le channel {channel}")
-
-            # Enregistrement
-            try:
-                save_result = await self._save_from_channel(
-                    channel, count, before=oldest_msg
-                )
-                await msg_bot.edit(
-                    content=f"**{channel.name}**\n{save_result} ({oldest_msg.created_at})"
-                )
-            except discord.Forbidden as exc:
-                await msg_bot.edit(content=f"**{channel.name}**\nErreur: {exc}")
-
-        await ctx.send("Fini !")
 
     async def _save_from_channel(
         self,
@@ -359,21 +321,6 @@ class History(commands.Cog):
         if nb_hidden_channels:
             channels_str += "\n" + f"*+ {nb_hidden_channels} autre(s) salon(s)*"
         return channels_str
-
-    async def _get_known_channels(self, db: Database) -> List[discord.TextChannel]:
-        """Récupérer la liste des channels connus en db"""
-        known_channels = []
-        with db.bind_ctx([Message]):
-            for known_channel_id in Message.select(Message.channel_id).distinct():
-                known_channel = self.bot.get_channel(known_channel_id.channel_id)
-                if not isinstance(known_channel, discord.TextChannel):
-                    print(
-                        f"Impossible de déterminer le channel correspondant à l'id {known_channel_id}"
-                    )
-                    continue
-                known_channels.append(known_channel)
-
-        return known_channels
 
 
 async def setup(bot):
